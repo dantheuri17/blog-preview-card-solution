2bdbf6542bf8ae0058a2972b0af47d61b4e98dbc
Daniel Theuri <dantheuri17@gmail.com>
2024-01-02 23:23:33 +0300

    changing font family

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import profileAvatar from "./assets/images/image-avatar.webp";
 
 function App() {
 	return (
-		<div className="flex items-center justify-center w-screen h-screen font-Figtree bg-blog-yellow">
+		<div className="flex items-center justify-center w-screen h-screen font-figtree bg-blog-yellow">
 			<div className="preview-card p-[1.5rem] grid grid-cols-1 gap-y-[24px] rounded-[1.25rem] bg-white w-[384px] h-[522px]">
 				<div className="flex justify-center w-full image-container">
 					<img src={illustrationArticle} className="w-full" alt="" />
