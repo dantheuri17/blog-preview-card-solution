54da9ff9b3bd60202c0e4a4d8feda539d62d2d15
Daniel Theuri <dantheuri17@gmail.com>
2024-01-03 00:38:27 +0300

    reduced border and smaller profile

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import profileAvatar from "./assets/images/image-avatar.webp";
 function App() {
 	return (
 		<div className="flex items-center justify-center w-screen h-screen font-figtree bg-blog-yellow">
-			<div className="preview-card border-[2px] border-black shadow-[8px_8px_0_0_rgb(0,0,0)] p-[1.5rem] grid grid-cols-1 gap-y-[24px] rounded-[1.25rem] bg-white w-[384px] h-[522px]">
+			<div className="preview-card border-[1px] border-black shadow-[8px_8px_0_0_rgb(0,0,0)] p-[1.5rem] grid grid-cols-1 gap-y-[1.5rem] rounded-[1.25rem] bg-white w-[384px] h-[522px]">
 				<div className="flex justify-center w-full image-container">
 					<img src={illustrationArticle} className="w-full" alt="" />
 				</div>
-				<div className="grid grid-cols-1 gap-y-[12px]">
+				<div className="grid grid-cols-1 gap-y-[.75rem]">
 					<div className="flex justify-center w-[82px] h-[29px] px-[12px] py-[4px] bg-blog-yellow">
 						<p className="font-bold text-[14px] leading-[21px]">Learning</p>
 					</div>
@@ -23,7 +23,7 @@ function App() {
 				</div>
 
 				<span className="flex gap-[12px] items-center">
-					<div className="w-10">
+					<div className="w-[2rem] height-[2rem] rounded-[2rem]">
 						<img src={profileAvatar} alt="" />
 					</div>
 					<h2 className="font-[800] text-[.875rem] leading-[1.3125rem]">
